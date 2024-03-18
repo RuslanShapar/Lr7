9ecef999667c1fe038f403612bf5f0874368d4f9
Ruslan <Ruslan@DESKTOP-N2A3VCS>
2024-03-18 20:51:55 +0200

    Добавьте файлы проекта.

---
+++ Lr7/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourNamespace.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View("~/wwwroot/DownloadFile.html");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DownloadFile(string firstName, string lastName, string fileName)
+        {
+            // Перевірка наявності введених даних
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(fileName))
+            {
+                ViewBag.Message = "Будь ласка, заповніть всі поля";
+                return View();
+            }
+
+            // Побудова шляху до файлу
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", fileName + ".txt");
+
+            // Запис інформації у файл
+            using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                await writer.WriteLineAsync($"Ім'я: {firstName}");
+                await writer.WriteLineAsync($"Прізвище: {lastName}");
+            }
+
+            // Повернення файлу користувачеві
+            byte[] fileBytes = await System.IO.File.ReadAllBytesAsync(filePath);
+            return File(fileBytes, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
---
+++ Lr7/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Lr7
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Додати сервіси, необхідні для додатку
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // Обробити помилки у виробничому середовищі
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
